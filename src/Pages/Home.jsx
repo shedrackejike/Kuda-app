6ccbbbc76a17fae154a0c4acd3a56d27f54ec08d
shedrackejike <shedrackejike3@gmail.com>
2023-09-19 16:50:48 +0100

    updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,6 +4,7 @@ import { BsFillCreditCardFill } from "react-icons/bs";
 import { FaBalanceScaleRight, FaPaperPlane } from "react-icons/fa";
 import { HiShieldExclamation } from "react-icons/hi";
 import { WiMoonAltWaxingGibbous1 } from "react-icons/wi";
+import { LuChevronRight } from "react-icons/lu";
 
 const Home = () => {
   return (
@@ -207,35 +208,39 @@ const Home = () => {
         </Flex>
       </Box>
 
-      <Box bg={"yellow"} border={"1px"} w={"90%"} margin={"auto"} h={"90%"}>
-        <Flex border={"1px"} w={"90%"} margin={"auto"} h={"90%"}>
-          <Box border={"1px"} w={"90%"} margin={"auto"} h={"90%"}>
-            <Box border={'1px'} w={'50%'} h={'60%'}>
-            <Text fontSize={'44.5px'}>
-            Your phone +
-            our app +
-            a debit card =
-            a simpler life.
-
-            </Text>
+      <Box w={"90%"} margin={"auto"} h={"90%"}>
+        <Flex w={"90%"} margin={"auto"} h={"90%"}>
+          <Box w={"90%"} margin={"auto"} h={"90%"}>
+            <Box w={"50%"} h={"60%"}>
+              <Text fontSize={"42px"} fontWeight={"bold"} textColor={"#57357F"}>
+                Your phone + our app + a debit card = a simpler life.
+              </Text>
             </Box>
 
-            <Box border={'1px'} w={'90%'} h={'20%'} mt={2}>
-                <Text>
-                We designed a money app for your lifestyle, and you can get a debit card to go with it. That's all you need to make the right money moves.
+            <Box w={"70%"} h={"20%"} mt={2}>
+              <Text>
+                We designed a money app for your lifestyle, and you can get a
+                debit card to go with it. That's all you need to make the right
+                money moves.
+              </Text>
+            </Box>
 
-
+            <Flex w={"70%"} h={"13%"} mt={"3"}>
+              <Link>
+                <Text
+                  fontWeight={"bold"}
+                  textColor={"#57357F"}
+                  justifyContent={"center"}
+                  alignItems={"center"}
+                  display={"flex"}
+                >
+                  Open An Account in Minutes <LuChevronRight />
                 </Text>
-            </Box>
-
-            <Box border={'1px'} w={'70%'} h={'13%'} mt={'3'}>
-                <Link>
-                <Text>Open An Account in Minutes </Text>
-                </Link>
-            </Box>
+              </Link>
+            </Flex>
           </Box>
 
-          <Box border={"1px"} w={"90%"} margin={"auto"} h={"90%"}>
+          <Box w={"90%"} margin={"auto"} h={"90%"}>
             <Image
               objectFit="cover"
               src="/images/phone.png"
@@ -246,6 +251,242 @@ const Home = () => {
           </Box>
         </Flex>
       </Box>
+
+      <Box w={"90%"} margin={"auto"} h={"80%"}>
+        <Flex
+          w={"90%"}
+          h={"90%"}
+          justifyContent={"space-evenly"}
+          alignItems={"center"}
+        >
+          <Box w={"50%"} h={"90%"}>
+            <Image
+              objectFit="cover"
+              src="/images/money.png"
+              alt="logo"
+              width={"fit-content"}
+              boxSize="100%"
+            />
+          </Box>
+
+          <Box
+            w={"45%"}
+            h={"90%"}
+            alignItems={"center"}
+            justifyContent={"center"}
+            display={"flex"}
+            flexDirection={"column"}
+          >
+            <Box h={"25%"} w={"80%"}>
+              <Text fontSize={"30px"} fontWeight={"bold"} textColor={"#57357F"}>
+                It’s your money, we just help you manage it.
+              </Text>
+            </Box>
+
+            <Box h={"25%"} w={"77%"} mt={"2"}>
+              <Text fontSize={"15.4px"}>
+                Save it, spend it, send it. It’s up to you. Whatever you choose
+                to do with your money, we’ll make sure it’s done better and free
+                of charge. We take responsibility for that.
+              </Text>
+            </Box>
+          </Box>
+        </Flex>
+      </Box>
+
+      <Box w={"90%"} margin={"auto"} h={"80%"}>
+        <Flex
+          h={"95%"}
+          w={"95%"}
+          margin={"auto"}
+          alignItems={"center"}
+          justifyContent={"space-evenly"}
+        >
+          <Box w={"45%"} h={"60%"}>
+            <Box h={"35%"} w={"70%"}>
+              <Text fontSize={"27px"} fontWeight={"bold"} textColor={"#57357F"}>
+                Save money as you spend it, seriously.
+              </Text>
+            </Box>
+            <Box h={"35%"} w={"90%"} mt={"2"}>
+              <Text>
+                You can’t avoid spending. That’s how you pay for your needs. But
+                we can help you put money away every time you pay for something.
+                Just set a percentage to save and watch your money grow.
+              </Text>
+            </Box>
+            <Flex h={"18%"} w={"70%"} mt={"2"}>
+              <Link>
+                <Text
+                  textColor={"#57357F"}
+                  justifyContent={"center"}
+                  alignItems={"center"}
+                  display={"flex"}
+                >
+                  See all our Savings <LuChevronRight />
+                </Text>
+              </Link>
+            </Flex>
+          </Box>
+
+          <Box w={"45%"} h={"85%"}>
+            <Image
+              objectFit="inherit"
+              src="/images/smart.png"
+              alt="logo"
+              width={"fit-content"}
+              boxSize="100%"
+            />
+          </Box>
+        </Flex>
+      </Box>
+
+
+
+
+      <Box w={"90%"} margin={"auto"} h={"80%"}>
+        <Flex
+          h={"95%"}
+          w={"95%"}
+          margin={"auto"}
+          alignItems={"center"}
+          justifyContent={"space-between"}
+        >
+          <Box w={"45%"} h={"85%"}>
+            <Image
+              objectFit="inherit"
+              src="/images/turn.png"
+              alt="logo"
+              width={"fit-content"}
+              boxSize="100%"
+            />
+          </Box>
+
+          <Box w={"45%"} h={"60%"}>
+            <Box h={"39%"} w={"80%"}>
+              <Text fontSize={"36px"} fontWeight={"bold"} textColor={"#57357F"}>
+                Turn off access, turn on safety.
+              </Text>
+            </Box>
+            <Box h={"35%"} w={"74.9%"} mt={"2"}>
+              <Text>
+                Life happens. Milk spills. Debit cards go missing. If that ever
+                happens, you can block your missing card on the app so no one
+                can use it. We’d like to see them try.
+              </Text>
+            </Box>
+            <Flex h={"18%"} w={"70%"} mt={"2"}>
+              <Link>
+                <Text
+                  textColor={"#57357F"}
+                  justifyContent={"center"}
+                  alignItems={"center"}
+                  display={"flex"}
+                >
+                  Learn more about Cards <LuChevronRight />
+                </Text>
+              </Link>
+            </Flex>
+          </Box>
+        </Flex>
+      </Box>
+
+      <Box w={"90%"} margin={"auto"} h={"80%"}>
+        <Flex
+          h={"95%"}
+          w={"95%"}
+          margin={"auto"}
+          alignItems={"center"}
+          justifyContent={"space-evenly"}
+        >
+          <Box w={"45%"} h={"60%"}>
+            <Box h={"39%"} w={"80%"}>
+              <Text fontSize={"37px"} fontWeight={"bold"} textColor={"#57357F"}>
+                We’re always happy to help you.
+              </Text>
+            </Box>
+            <Box h={"35%"} w={"80%"} mt={"2"}>
+              <Text>
+                You can chat with us on the app, slide into our DMs, tweet,
+                leave an Instagram comment, send an email or call. However you
+                choose to reach out, there’ll always be a friendly person there
+                to make your life easy.
+              </Text>
+            </Box>
+            <Flex h={"18%"} w={"70%"} mt={"2"}>
+              <Link>
+                <Text
+                  textColor={"#57357F"}
+                  justifyContent={"center"}
+                  alignItems={"center"}
+                  display={"flex"}
+                >
+                  Get Help <LuChevronRight />
+                </Text>
+              </Link>
+            </Flex>
+          </Box>
+
+          <Box w={"45%"} h={"85%"}>
+            <Image
+              objectFit="inherit"
+              src="/images/Hello.png"
+              alt="logo"
+              width={"fit-content"}
+              boxSize="100%"
+            />
+          </Box>
+        </Flex>
+      </Box>
+
+
+      <Box  w={"90%"} margin={"auto"} h={"80%"}>
+        <Flex
+      
+          h={"95%"}
+          w={"95%"}
+          margin={"auto"}
+          alignItems={"center"}
+          justifyContent={"space-between"}
+        >
+          <Box  w={"40%"} h={"90%"}>
+            <Image
+              objectFit="inherit"
+              src="/images/fees.png"
+              alt="logo"
+              width={"fit-content"}
+              boxSize="100%"
+            />
+          </Box>
+
+          <Box  w={"45%"} h={"40%"}>
+            <Box h={"39%"} w={"80%"}>
+              <Text fontSize={"36px"} fontWeight={"bold"} textColor={"#57357F"}>
+              Fees as clear as glass.
+              </Text>
+            </Box>
+            <Box  h={"35%"} w={"74.9%"} mt={"2"}>
+              <Text>
+              We’re serious about free banking, and we will never, ever charge you for anything without your consent.
+              </Text>
+            </Box>
+            <Flex  h={"18%"} w={"70%"} mt={"2"}>
+              <Link>
+                <Text
+                  textColor={"#57357F"}
+                  justifyContent={"center"}
+                  alignItems={"center"}
+                  display={"flex"}
+                >
+                  See all our fees <LuChevronRight />
+                </Text>
+              </Link>
+            </Flex>
+          </Box>
+        </Flex>
+      </Box>
+
+
     </Box>
   );
 };
