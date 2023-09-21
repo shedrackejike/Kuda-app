11072048e595a070dae270685038cc9ee71c0e10
shedrackejike <shedrackejike3@gmail.com>
2023-09-21 16:39:34 +0100

    updated

---
+++ src/utils/money.js
@@ -0,0 +1,9 @@
+export const moneyData = [
+    {
+        id: "1",
+        app: "The money app for Africans.",
+        save: "Save, spend, send and invest money across borders.",
+        button: "Join Kuda",
+        image: "images/mood.png"
+    }
+]
