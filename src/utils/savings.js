1e9a59ec84b26175afcc5e479d61d5edf7945998
shedrackejike <shedrackejike3@gmail.com>
2023-10-10 14:56:06 +0100

    updated

--- src/utils/savings.js
+++ src/utils/savings.js
@@ -1,17 +1,17 @@
 export const savinsData = [
     {
         id: "1",
-        icon: "",
+        icon: "AiFillCreditCard",
         Comment: "Get up to 15% annual interest on Fixed Savings."
     },
     {
         id: "2",
-        icon: "",
+        icon: "GiBeard",
         Comment: "Save every time you spend from your account with Spend+Save."
     },
     {
         id: "3",
-        icon: "",
+        icon: "FaCreativeCommonsZero",
         Comment: "Spend nothing on ridiculous bank charges."
     },
 ]
