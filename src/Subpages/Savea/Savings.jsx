c0d6786e320592e5c38bd249905a4166df82dc1b
shedrackejike <shedrackejike3@gmail.com>
2023-10-10 16:18:27 +0100

    updated

--- src/Subpages/Savea/Savings.jsx
+++ src/Subpages/Savea/Savings.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import { Box, Flex, Text } from '@chakra-ui/react'
+import React, { useEffect, useState } from 'react'
+
+import { savinsData } from '../../utils/savings'
 
 const Savings = () => {
+    const [savings,setSavings] = useState([]) 
+    
+    useEffect(() =>(
+        setSavings(savinsData)
+    ),[])
+
+
   return (
-    <div>Savings</div>
+    <Box minH={'300px'} w={'90%'}  margin={'auto'}  alignItems={'center'} justifyContent={'center'} display={'flex'}>
+
+       {savings?.map(data =>(
+         <Flex minH={'200px'} w={'97%'} justifyContent={'space-around'}  alignItems={'center'}>
+
+         <Box minH={"60px"} w={"330px"} boxShadow="2xl" p="8" rounded="md" bg="white">
+             <Box
+               h={"7"}
+               w={"10%"}
+               mb={"2"}
+               marginLeft={"4"}
+               borderRadius={"50%"}
+               alignItems={"center"}
+               justifyContent={"center"}
+               display={"flex"}
+               bg={"#DFE3FF"}
+             >
+                
+               {data.icon}
+             </Box>
+ 
+             <Box minH={"60%"} w={"90%"} margin={"auto"}>
+               <Text textColor={"#57357F"} fontSize={"13"} fontWeight={"bold"}>
+                {data.Comment}
+               </Text>
+             </Box>
+           </Box>
+ 
+          
+ 
+          
+         </Flex>
+       ))}
+
+    </Box>
   )
 }
 
--- src/utils/savings.js
+++ src/utils/savings.js
@@ -1,17 +1,22 @@
+import { AiFillCreditCard} from 'react-icons/ai'
+import {FaCreativeCommonsZero } from 'react-icons/fa'
+import {GiBeard} from "react-icons/gi"
+import { Icon } from '@chakra-ui/react'
+
 export const savinsData = [
     {
         id: "1",
-        icon: "AiFillCreditCard",
+        icon:   <Icon as={AiFillCreditCard}/>,
         Comment: "Get up to 15% annual interest on Fixed Savings."
     },
     {
         id: "2",
-        icon: "GiBeard",
+        icon: <Icon as={GiBeard}/>,
         Comment: "Save every time you spend from your account with Spend+Save."
     },
     {
         id: "3",
-        icon: "FaCreativeCommonsZero",
+        icon:  <Icon as={FaCreativeCommonsZero} />,
         Comment: "Spend nothing on ridiculous bank charges."
     },
 ]
