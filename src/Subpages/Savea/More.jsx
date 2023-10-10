93144027192ca518a61626d1ccfa602d5c827a0b
shedrackejike <shedrackejike3@gmail.com>
2023-10-10 14:40:04 +0100

    updated

--- src/Subpages/Savea/More.jsx
+++ src/Subpages/Savea/More.jsx
@@ -1,29 +1,51 @@
-import { Box, Flex } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Flex,Text } from '@chakra-ui/react'
+import React, { useEffect, useState } from 'react'
+import { saveDate } from '../../utils/save'
 
 const More = () => {
+    const [save,setSave] = useState([])
+
+    useEffect(() => (
+        setSave(saveDate)
+    ),[])
   return (
-    <Box minH={"550px"} w={'90%'} bg={'green'} margin={'auto'}>
-        <Flex minH={'500px'} w={'100%'} border={'1px'} >
-            <Box minH={'200px'} w={'50%'} border={'1px'}>
-                <Box h={'80px'} w={'90px'} border={'1px'}>
+    <Box minH={"550px"} w={'90%'} margin={'auto'}>
+        {save?.map((data)=>(
+        <Flex minH={'500px'} w={'100%'}  justifyContent={'space-around'} alignItems={'center'} >
 
+            
+             <Box minH={'250px'} w={'40%'} >
+                <Box minH={'80px'} w={'390px'} >
+                    <Text fontSize={'45px'} fontWeight={'bold'} textColor={'#40196D'}>{data.head}</Text>
                 </Box>
 
-                <Box>
-
+                <Box minH={'80px'} w={'400px'}  mt={'3'}>
+                    <Text>{data.Comment}</Text>
                 </Box>
 
-                <Box>
-
+                <Box minH={'40px'} w={'120px'}  mt={'3'} alignItems={'center'} justifyContent={'center'} display={'flex'} bg={'#40196D'} borderRadius={'7'}>
+                    <Text textColor={'white'}>{data.button}</Text>
                 </Box>
 
             </Box>
 
-            <Box minH={'470px'} w={'50%'} border={'1px'}>
+            <Box minH={'470px'} w={'50%'}
+             justify={"center"}
+             alignItems={"center"}
+             backgroundImage={data.Image}  
+             backgroundRepeat={"no-repeat"}
+             backgroundPosition={"center"}
+             backgroundSize={"contain"}
+             display={"flex"}
+             margin={"auto"}
+            >
 
             </Box>
+            
+        
+
         </Flex>
+        ))}
     </Box>
   )
 }
