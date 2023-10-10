f07f0f665ef1339ed42479f4a821bb3f9c8b0a14
shedrackejike <shedrackejike3@gmail.com>
2023-10-10 14:51:38 +0100

    updated

--- src/Pages/Save.jsx
+++ src/Pages/Save.jsx
@@ -1,11 +1,13 @@
 import { Box } from '@chakra-ui/react'
 import React from 'react'
 import More from '../Subpages/Savea/More'
+import Savings from '../Subpages/Savea/Savings'
 
 const Save = () => {
   return (
    <Box>
-     <More/>   
+     <More/>
+     <Savings/>   
    </Box>
   )
 }
---
+++ src/utils/savings.js
@@ -0,0 +1,17 @@
+export const savinsData = [
+    {
+        id: "1",
+        icon: "",
+        Comment: "Get up to 15% annual interest on Fixed Savings."
+    },
+    {
+        id: "2",
+        icon: "",
+        Comment: "Save every time you spend from your account with Spend+Save."
+    },
+    {
+        id: "3",
+        icon: "",
+        Comment: "Spend nothing on ridiculous bank charges."
+    },
+]
