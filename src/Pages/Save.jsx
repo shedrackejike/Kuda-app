2a12f57361c4dec0f00598ffa3f60de99e6a41f4
shedrackejike <shedrackejike3@gmail.com>
2023-10-11 10:23:04 +0100

    updated

--- src/Pages/Save.jsx
+++ src/Pages/Save.jsx
@@ -2,12 +2,14 @@ import { Box } from '@chakra-ui/react'
 import React from 'react'
 import More from '../Subpages/Savea/More'
 import Savings from '../Subpages/Savea/Savings'
+import Real from '../Subpages/Savea/Real'
 
 const Save = () => {
   return (
    <Box>
      <More/>
      <Savings/>   
+    <Real/>
    </Box>
   )
 }
---
+++ src/utils/real.js
@@ -0,0 +1,9 @@
+export const realData = [
+    {
+        id: "1",
+        head: "Effortless saving is a real thing.",
+        Comment: "You can’t avoid spending. That’s how you pay for your needs. But we can help you put money away every time you pay for something. Just set a percentage to save and watch your money grow with Spend+Save",
+        button: "See how Spend + Save works",
+        image: "images/smart.png"
+    }
+]
