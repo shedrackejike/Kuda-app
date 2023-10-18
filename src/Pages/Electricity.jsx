79b6621278e227b3b677a64641419610073011e7
shedrackejike <shedrackejike3@gmail.com>
2023-10-18 12:50:43 +0100

    updated

--- src/Pages/Electricity.jsx
+++ src/Pages/Electricity.jsx
@@ -2,12 +2,14 @@ import { Box } from "@chakra-ui/react";
 import React from "react";
 import Cityapp from "../Subpages/Electricity/Cityapp";
 import Buy from "../Subpages/Electricity/Buy";
+import Meter from "../Subpages/Electricity/Meter";
 
 const Electricity = () => {
   return (
     <Box>
       <Cityapp />
       <Buy />
+      <Meter />
     </Box>
   );
 };
--- src/utils/meter.js
+++ src/utils/meter.js
@@ -1,10 +1,10 @@
 export const meterData = [
   {
     id: "1",
-    head: "Meet your money goals every time.",
+    head: "Meter units finished at midnight? Buy more on the Kuda app.",
     Comment:
-      "Save daily, weekly or monthly, that’s up to you. Whichever way, our Flexible Savings plan will make it a breeze to put money away regularly.",
-    button: "Learn more about Flexible Savings >",
-    image: "images/want.png",
+      "Kuda takes the stress out of recharging your prepaid meter whenever you need to.",
+    button: "Join Kuda >",
+    image: "images/meter.png",
   },
 ];
