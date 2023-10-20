fede36956e9cb15ee0db16912f2aaa2c0b283522
shedrackejike <shedrackejike3@gmail.com>
2023-10-20 05:05:40 +0100

    updated

--- src/Pages/Cardless.jsx
+++ src/Pages/Cardless.jsx
@@ -2,12 +2,14 @@ import { Box } from "@chakra-ui/react";
 import React from "react";
 import Leave from "../Subpages/Cardlessap/Leave";
 import Atms from "../Subpages/Transfer/Atms";
+import Pos from "../Subpages/Cardlessap/Pos";
 
 const Cardless = () => {
   return (
     <Box>
       <Leave />
       <Atms />
+      <Pos />
     </Box>
   );
 };
---
+++ src/utils/pos.js
@@ -0,0 +1,9 @@
+export const posData = [
+  {
+    id: "1",
+    head: "You don’t need a card to pay on a POS.",
+    Comment: "Use Pay With USSD on Kuda to make easy cardless POS payments.",
+    button: "See How Pay With USSD Works > ",
+    Image: "images/pos.png",
+  },
+];
