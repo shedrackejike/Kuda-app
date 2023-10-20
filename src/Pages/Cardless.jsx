40ee677f5f3a9e6594685b4555fbf3ba993e1c7a
shedrackejike <shedrackejike3@gmail.com>
2023-10-20 06:36:12 +0100

    updated

--- src/Pages/Cardless.jsx
+++ src/Pages/Cardless.jsx
@@ -5,6 +5,8 @@ import Atms from "../Subpages/Transfer/Atms";
 import Pos from "../Subpages/Cardlessap/Pos";
 import Online from "../Subpages/Cardlessap/Online";
 import Forever from "../Subpages/Cardlessap/Forever";
+import Cardfqs from "../Subpages/Cardlessap/Cardfqs";
+import Money from "../Subpages/Home/Money";
 
 const Cardless = () => {
   return (
@@ -14,6 +16,8 @@ const Cardless = () => {
       <Pos />
       <Online />
       <Forever />
+      <Cardfqs />
+      <Money />
     </Box>
   );
 };
---
+++ src/utils/cardfqs.js
@@ -0,0 +1,36 @@
+export const cardfqstData = [
+  {
+    id: "1",
+    title: "What cashless payment options are on the Kuda app?",
+    Comment:
+      "Free transfers, Pay With Bank, Pay With USSD and cards are the cashless payment options on the Kuda app.",
+  },
+
+  {
+    id: "2",
+    title: "How many free transfers can I get on the Kuda app?",
+    Comment:
+      "When you sign up on Kuda with your BVN (Bank Verification Number), you’ll get 25 free transfers to all Nigerian banks every month.",
+  },
+
+  {
+    id: "3",
+    title: " Where does Pay With Bank work?",
+    Comment:
+      "Pay With Bank works on websites and apps that accept payments through Paystack. Learn more about Pay With Bank here.",
+  },
+
+  {
+    id: "1",
+    title: "Where does Pay With USSD work?",
+    Comment:
+      "Pay With USSD works on POS terminals and on websites that accept payments through Paystack, Flutterwave or Remita. Learn more about Pay With USSD here.",
+  },
+
+  {
+    id: "1",
+    title: "What kind of card can I get on the Kuda app?",
+    Comment:
+      "You can request a physical Kuda Visa naira card and a virtual Kuda Visa naira card. Learn more about Kuda Visa cards here.",
+  },
+];
