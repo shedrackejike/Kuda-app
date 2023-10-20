b33810c92cc4d77656bda27c2bc7e571749c6911
shedrackejike <shedrackejike3@gmail.com>
2023-10-20 03:54:30 +0100

    updated

--- src/Pages/Giftcard.jsx
+++ src/Pages/Giftcard.jsx
@@ -4,6 +4,7 @@ import Gift from "../Subpages/Giftcard/Gift";
 import Network from "../Subpages/Airtime/Network";
 import Dollar from "../Subpages/Giftcard/Dollar";
 import Game from "../Subpages/Giftcard/Game";
+import Treat from "../Subpages/Giftcard/Treat";
 
 const Giftcard = () => {
   return (
@@ -12,6 +13,7 @@ const Giftcard = () => {
       <Network />
       <Dollar />
       <Game />
+      <Treat />
     </Box>
   );
 };
---
+++ src/utils/treat.js
@@ -0,0 +1,10 @@
+export const treatData = [
+  {
+    id: "1",
+    head: "Treat yourself or someone else in minutes.",
+    Comment:
+      "Buy a gift card for yourself or someone else on the Kuda app and it’ll automatically be delivered by email. ",
+    button: "Download Kuda >",
+    Image: "images/treat.png",
+  },
+];
