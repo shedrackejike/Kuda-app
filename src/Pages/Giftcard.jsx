250ca68476a9aa66bcdf56026cff562d0d6310ef
shedrackejike <shedrackejike3@gmail.com>
2023-10-20 02:18:50 +0100

    updated

--- src/Pages/Giftcard.jsx
+++ src/Pages/Giftcard.jsx
@@ -2,12 +2,14 @@ import { Box } from "@chakra-ui/react";
 import React from "react";
 import Gift from "../Subpages/Giftcard/Gift";
 import Network from "../Subpages/Airtime/Network";
+import Dollar from "../Subpages/Giftcard/Dollar";
 
 const Giftcard = () => {
   return (
     <Box>
       <Gift />
       <Network />
+      <Dollar />
     </Box>
   );
 };
