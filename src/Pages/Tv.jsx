bc9924a070ea0a2d7ce722ed2ef35ccd0c0b473f
shedrackejike <shedrackejike3@gmail.com>
2023-10-20 12:27:38 +0100

    updated

--- src/Pages/Tv.jsx
+++ src/Pages/Tv.jsx
@@ -2,12 +2,14 @@ import { Box } from "@chakra-ui/react";
 import React from "react";
 import Paytv from "../Subpages/Tv/Paytv";
 import Network from "../Subpages/Airtime/Network";
+import Without from "../Subpages/Tv/Without";
 
 const Tv = () => {
   return (
     <Box>
       <Paytv />
       <Network />
+      <Without />
     </Box>
   );
 };
---
+++ src/utils/without.js
@@ -0,0 +1,10 @@
+export const withoutData = [
+  {
+    id: "1",
+    head: "Pay on foreign stores without a dollar card.",
+    Comment:
+      "Shop abroad with gift cards for popular online stores like Amazon, Walmart and Best Buy on the Kuda app.",
+    button: "Join Kuda >",
+    Image: "images/without.png",
+  },
+];
